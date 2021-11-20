f915bdf5130410c813623319fe057fd676065902
evgeny-dyakov <e3001dk@gmail.com>
2021-11-20 20:55:07 +0300

    фиксирует изменения по questions

--- js/script.js
+++ js/script.js
@@ -1,6 +1,29 @@
 $(document).ready(function() {
   $('.questions__slider').slick({
-    // arrows: false
-    dots: true
+    infinite: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    variableWidth: true,
+    waitForAnimate: false,
+    responsive: [
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 3
+        }
+      }, {
+        breakpoint: 955,
+        settings: {
+          slidesToShow: 2
+        }
+      }, {
+        breakpoint: 628,
+        settings: {
+          slidesToShow: 1,
+          arrows: false,
+          dots: true
+        }
+      }
+    ]
   });
 });
