a3e8317095cbb9607ae4f68bb506e62633e94221
evgeny-dyakov <e3001dk@gmail.com>
2021-11-22 09:28:28 +0300

    вносит изменения в работу слайдера

--- js/script.js
+++ js/script.js
@@ -1,33 +1,37 @@
 $(document).ready(function() {
   $('.questions__slider').slick({
     arrows: true,
-    infinite: false,
+    infinite: true,
     slidesToShow: 4,
-    slidesToScroll: 1,
+    slidesToScroll: 2,
     variableWidth: true,
     responsive: [
       {
         breakpoint: 1280,
         settings: {
-          slidesToShow: 3
+          slidesToShow: 3,
+          slidesToScroll: 2
         }
       },
       {
         breakpoint: 955,
         settings: {
-          slidesToShow: 2
+          slidesToShow: 2,
+          slidesToScroll: 1
         }
       },
       {
         breakpoint: 628,
         settings: {
-          slidesToShow: 1
+          slidesToShow: 1,
+          slidesToScroll: 1
         }
       },
       {
         breakpoint: 440,
         settings: {
           slidesToShow: 1,
+          slidesToScroll: 1,
           dots: true,
           arrows: false
         }
